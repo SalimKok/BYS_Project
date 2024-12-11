b3e6f0138ba7660f7f4aab8147912a7c163cefe0
Salim kök <155094272+SalimKok@users.noreply.github.com>
2024-12-11 22:50:56 +0300

    start of my project

---
+++ BYS_Poject/controllers/SglConnectionClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BYS_Poject.controllers
+{
+    public class SglConnectionClass
+    {
+            public static SqlConnection connection = new SqlConnection("Data Source=DESKTOP-9MRP4SI\\SQLEXPRESS;Initial Catalog=BYS.data;Integrated Security=True;Encrypt=False");
+
+            public static void CheckConnection()
+            {
+                if (connection.State == System.Data.ConnectionState.Closed)
+                {
+                    connection.Open();
+                }
+                else { }
+            }
+        }
+    }
+
+        
